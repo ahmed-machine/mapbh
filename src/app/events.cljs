7ba3d81502850b1df12e99f3d8c54984d3e63502
ahmed-machine <ahmed-machine@users.noreply.github.com>
2025-07-24 15:04:55 -0400

    Fix language switching and cleanup url

--- src/app/events.cljs
+++ src/app/events.cljs
@@ -1,13 +1,43 @@
 (ns app.events
   (:require [re-frame.core :as re-frame]
-             [app.model :as model]))
+             [app.model :as model]
+             [bidi.bidi :as bidi]))
 
 (re-frame/reg-event-db
  ::set-active-panel
  (fn [db [_ active-panel]]
    (assoc db :active-panel (keyword active-panel))))
 
-(re-frame/reg-event-db
+(defn ^:private get-current-panel-from-url
+  "Extract current panel from URL as fallback when active-panel is unreliable"
+  []
+  (try
+    (let [pathname (-> js/window .-location .-pathname)
+          matched (bidi/match-route model/routes pathname)]
+      (when matched (:handler matched)))
+    (catch js/Error _
+      :home)))
+
+(re-frame/reg-event-fx
  ::set-language
- (fn [db [_ language]]
-   (assoc db :language (keyword language))))
+ (fn [{:keys [db]} [_ language]]
+   (let [current-panel (or (get-current-panel-from-url)
+                           (:active-panel db)
+                           :home)
+         new-db (assoc db :language (keyword language))]
+     (try
+       (let [current-search (-> js/window .-location .-search)
+             new-url (str (bidi/path-for model/routes current-panel :language (keyword language))
+                         current-search)]
+         {:db new-db
+          :history-push new-url})
+       (catch js/Error e
+         (.warn js/console "Language switch failed:" e)
+         {:db new-db})))))
+
+;; Effect handler for navigation using History API
+(re-frame/reg-fx
+ :history-push
+ (fn [url]
+   ;; Just update the URL without triggering navigation
+   (.pushState js/window.history nil "" url)))
