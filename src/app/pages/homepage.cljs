7e394b0401b98ad667a6cea9fce75dc0b6939fb7
ahmed-machine <ahmed-machine@users.noreply.github.com>
2025-07-25 13:15:03 -0400

    Homepage improvements (#16)
    * Homepage more response, cleaner
    
    * More responsive
    
    * Order About before articles
    
    * Fix spacing issues and cleanup CSS

--- src/app/pages/homepage.cljs
+++ src/app/pages/homepage.cljs
@@ -6,68 +6,77 @@
             [app.routes :as routes]))
 
 
-(def tagline "A visualization of Bahrain through historic maps.")
+(def tagline "Visualising Bahrain through historic maps.")
 (def ar-tagline "رحلة في تاريخ البحرين عبر الخرائط")
 
 (defn en []
   [:div.container.home
-   [:p.column.is-full.logo {:style {:font-size "5em"
-                                    :z-index 2
-                                    :margin-top "9%"
-                                    :margin-bottom "1%"
-                                    :text-align :center}} "mapBH"]
-   [:p.column.is-full {:style {:z-index 2
-                               :margin-bottom "1%"
-                               :text-align :center}} tagline]
-   [:nav.navbar.columns.is-centered.is-vcentered {:role "navigation" :style {:padding "1rem"}}
-    [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :about)} "About"]
-    [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :contribute)} "Contribute"]]
-   [:nav.navbar.columns.is-centered.is-vcentered {:role "navigation" :style {:padding "1rem"}}
-    [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :map)} "Map"]
-    [:a.navbar-item.column.is-rounded.is-2.is-centered.is-vcentered.button {:href (routes/url-for :article-index)} "Articles"]]
-   [:div.content.has-text-centered
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://twitter.com/map_bh"} [:i.fab.fa-twitter]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://github.com/AHAAAAAAA/mapbh"} [:i.fab.fa-github]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "mailto:mapbh.org@gmail.com"} [:i.fas.fa-envelope]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://instagram.com/map_bh"} [:i.fab.fa-instagram]]]]
-   [:a.column.is-full {:role "navigation"
-                       :lang "ar"
-                       :style {:text-align :center}
-                       :on-click #(rf/dispatch [::events/set-language "ar"])}
-    [:button.button.is-outlined.is-rounded.is-small {:style {:font-size "1em"}}  "العربية"]]])
+   [:div.main-content
+    [:div.has-text-centered
+     [:h1.logo "mapBH"]
+     [:p {:style {:color "var(--text-muted)" :margin-top "0.5rem"}} tagline]]
+    [:nav.navbar {:role "navigation"}
+     [:div.navbar-menu
+      [:div.primary-button-container
+       [:a.navbar-item.button.is-primary {:href (routes/url-for :map)} "Explore Maps"]]
+      [:div.secondary-buttons-container
+       [:a.navbar-item.button {:href (routes/url-for :about)} "About"]
+       [:a.navbar-item.button {:href (routes/url-for :article-index)} "Articles"]
+       [:a.navbar-item.button {:href (routes/url-for :contribute)} "Contribute"]]]]]
+   [:div.footer-content
+    [:div.content.has-text-centered {:style {:margin-top "var(--spacing-md)"}}
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://twitter.com/map_bh"
+                           :aria-label "Twitter"} [:i.fab.fa-twitter]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://github.com/AHAAAAAAA/mapbh"
+                           :aria-label "GitHub"} [:i.fab.fa-github]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "mailto:mapbh.org@gmail.com"
+                           :aria-label "Email"} [:i.fas.fa-envelope]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://instagram.com/map_bh"
+                           :aria-label "Instagram"} [:i.fab.fa-instagram]]]]
+    [:div.has-text-centered {:style {:margin-top "var(--spacing-lg)"}}
+     [:button.button.is-outlined.is-rounded.language-switch
+      {:role "navigation"
+       :lang "ar"
+       :style {:font-family "Amiri, Scheherazade, serif"}
+       :on-click #(rf/dispatch [::events/set-language "ar"])}
+      "العربية"]]]])
 
 
 (defn ar []
-  [:div.container.home
-   [:p.column.is-full.logo {:style {:font-size "5em"
-                                    :z-index 2
-                                    :margin-top "9%"
-                                    :margin-bottom "1%"
-                                    :text-align :center}} "mapBH"]
-   [:div {:dir "rtl" :lang "ar" :style {:font-size "1.25em"}}
-    [:p.column.is-full {:style {:z-index 2
-                                :margin-bottom "1%"
-                                :text-align :center}} ar-tagline]
-    [:nav.navbar.columns.is-centered.is-vcentered {:role "navigation" :style {:padding "1rem"}}
-     [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :about)} "نبذة"]
-     [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :contribute)} "ساهم"]]]
-   [:nav.navbar.columns.is-centered.is-vcentered {:role "navigation" :style {:padding "1rem"}}
-    [:a.navbar-item.column.is-rounded.is-2.is-centered.is-vcentered.button {:href (routes/url-for :article-index)} "مقالات"]
-    [:a.navbar-item.column.button.is-rounded.is-2.is-centered.is-vcentered {:href (routes/url-for :map)} "الخارطة"]]
-   [:div.content.has-text-centered {:style {:margin-top "1.1rem"}}
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://twitter.com/map_bh"} [:i.fab.fa-twitter]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://github.com/AHAAAAAAA/mapbh"} [:i.fab.fa-github]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "mailto:mapbh.org@gmail.com"} [:i.fas.fa-envelope]]]
-    [:span.icon.home [:a {:style {:color :black}
-                          :href "https://instagram.com/map_bh"} [:i.fab.fa-instagram]]]]
-   [:a.column.is-full {:role "navigation"
-                       :style {:text-align :center}
-                       :on-click #(rf/dispatch [::events/set-language "en"])}
-    [:button.button.is-outlined.is-rounded.is-small "English"]]])
+  [:div.container.home {:dir "rtl" :lang "ar"}
+   [:div.main-content
+    [:div.has-text-centered
+     [:h1.logo "mapBH"]
+     [:p {:style {:color "var(--text-muted)" :margin-top "0.5rem"}} ar-tagline]]
+    [:nav.navbar {:role "navigation"}
+     [:div.navbar-menu
+      [:div.primary-button-container
+       [:a.navbar-item.button.is-primary {:href (routes/url-for :map)} "استكشف الخرائط"]]
+      [:div.secondary-buttons-container
+       [:a.navbar-item.button {:href (routes/url-for :about)} "نبذة عن المشروع"]
+       [:a.navbar-item.button {:href (routes/url-for :article-index)} "مقالات"]
+       [:a.navbar-item.button {:href (routes/url-for :contribute)} "ساهم"]]]]]
+   [:div.footer-content
+    [:div.content.has-text-centered {:style {:margin-top "var(--spacing-md)"}}
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://twitter.com/map_bh"
+                           :aria-label "تويتر"} [:i.fab.fa-twitter]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://github.com/AHAAAAAAA/mapbh"
+                           :aria-label "جيت هب"} [:i.fab.fa-github]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "mailto:mapbh.org@gmail.com"
+                           :aria-label "بريد إلكتروني"} [:i.fas.fa-envelope]]]
+     [:span.icon.home [:a {:style {:color "var(--text-muted)"}
+                           :href "https://instagram.com/map_bh"
+                           :aria-label "إنستغرام"} [:i.fab.fa-instagram]]]]
+    [:div.has-text-centered {:style {:margin-top "var(--spacing-lg)"}}
+     [:button.button.is-outlined.is-rounded.language-switch
+      {:role "navigation"
+       :style {:font-family "Roboto, Helvetica, sans-serif"}
+       :on-click #(rf/dispatch [::events/set-language "en"])}
+      "English"]]]])
