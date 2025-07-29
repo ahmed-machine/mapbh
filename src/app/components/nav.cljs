eb62130f715e64f61213bda926d866edc2063108
Ahmed <ahmed-machine@users.noreply.github.com>
2025-07-29 15:24:22 -0400

    navbar clean up

--- src/app/components/nav.cljs
+++ src/app/components/nav.cljs
@@ -1,43 +1,56 @@
 (ns app.components.nav
   (:require [app.routes :as routes]
             [app.events :as events]
+            [app.model :as model]
             [re-frame.core :as rf]))
 
 (defn top-en
   []
-  [:nav.navbar.is-fixed-top
-   [:div.navbar-brand
-    [:a.nav-logo.is-vcentered {:href (routes/url-for :home)}
-     [:p.column.logo {:style {:color :black
-                              :z-index 2}} "mapBH"]]]
-   [:div.navbar-menu.is-active {:style {:font-size "0.8em"}}
-    [:div.navbar-start.is-vcentered
-     [:a.navbar-item {:href (routes/url-for :about)} "About"]
-     [:a.navbar-item {:href (routes/url-for :article-index)} "Articles"]
-     [:a.navbar-item {:href (routes/url-for :catalogue)} "Catalogue"]
-     [:a.navbar-item {:href (routes/url-for :contribute)} "Contribute"]
-     [:a.navbar-item {:href (routes/url-for :map)} "Map"]
-     [:a.navbar-item {:style {:font-family "Amiri, Scheherazade, serif" :display :flex :align-items :center}
-                      :on-click #(rf/dispatch [::events/set-route-params {:language "ar"}])} "العربية"]]]])
+  (let [active-panel @(rf/subscribe [::model/active-panel])]
+    [:nav.navbar.is-fixed-top
+     [:div.navbar-brand
+      [:a.nav-logo.is-vcentered {:href (routes/url-for :home)}
+       [:p.column.logo {:style {:color :black
+                                :z-index 2}} "mapBH"]]]
+     [:div.navbar-menu.is-active {:style {:font-size "0.8em"}}
+      [:div.navbar-start.is-vcentered
+       [:a.navbar-item {:href (routes/url-for :about)
+                        :class (when (= active-panel :about) "is-active")} "About"]
+       [:a.navbar-item {:href (routes/url-for :article-index)
+                        :class (when (= active-panel :article-index) "is-active")} "Articles"]
+       [:a.navbar-item {:href (routes/url-for :catalogue)
+                        :class (when (= active-panel :catalogue) "is-active")} "Catalogue"]
+       [:a.navbar-item.contribute-button {:href (routes/url-for :contribute)
+                                          :class (when (= active-panel :contribute) "is-active")} "Contribute"]
+       [:a.navbar-item {:href (routes/url-for :map)
+                        :class (when (= active-panel :map) "is-active")} "Map"]
+       [:a.navbar-item {:style {:font-family "Amiri, Scheherazade, serif" :display :flex :align-items :center}
+                        :on-click #(rf/dispatch [::events/set-route-params {:language "ar"}])} "العربية"]]]]))
 
 
 (defn top-ar
   []
-  [:nav.navbar.is-fixed-top {:lang "ar" :direction "rtl"}
-   [:div.navbar-brand
-    [:a.nav-logo.is-vcentered {:href (routes/url-for :home)}
-     [:p.column.logo {:style {:color :black
-                              :font-family "Comfortaa"
-                              :z-index 2}} "mapBH"]]]
-   [:div.navbar-menu.is-active {:style {:font-size "1.1em"}}
-    [:div.navbar-end
-     [:a.navbar-item {:style {:font-family "Roboto, Helvetica, san serif" :font-size "0.8em" :display :flex :align-items :center}
-                      :on-click #(rf/dispatch [::events/set-route-params {:language "en"}])} "English"]
-     [:a.navbar-item {:href (routes/url-for :contribute)} "ساهم"]
-     [:a.navbar-item {:href (routes/url-for :catalogue)} "فهرس"]
-     [:a.navbar-item {:href (routes/url-for :article-index)} "مقالات"]
-     [:a.navbar-item {:href (routes/url-for :about)} "نبذة"]
-     [:a.navbar-item {:href (routes/url-for :map)} "الخارطة"]]]])
+  (let [active-panel @(rf/subscribe [::model/active-panel])]
+    [:nav.navbar.is-fixed-top {:lang "ar" :direction "rtl"}
+     [:div.navbar-brand
+      [:a.nav-logo.is-vcentered {:href (routes/url-for :home)}
+       [:p.column.logo {:style {:color :black
+                                :font-family "Comfortaa"
+                                :z-index 2}} "mapBH"]]]
+     [:div.navbar-menu.is-active {:style {:font-size "1.1em"}}
+      [:div.navbar-end
+       [:a.navbar-item {:style {:font-family "Roboto, Helvetica, san serif" :font-size "0.8em" :display :flex :align-items :center}
+                        :on-click #(rf/dispatch [::events/set-route-params {:language "en"}])} "English"]
+       [:a.navbar-item.contribute-button {:href (routes/url-for :contribute)
+                                          :class (when (= active-panel :contribute) "is-active")} "ساهم"]
+       [:a.navbar-item {:href (routes/url-for :catalogue)
+                        :class (when (= active-panel :catalogue) "is-active")} "فهرس"]
+       [:a.navbar-item {:href (routes/url-for :article-index)
+                        :class (when (= active-panel :article-index) "is-active")} "مقالات"]
+       [:a.navbar-item {:href (routes/url-for :about)
+                        :class (when (= active-panel :about) "is-active")} "نبذة"]
+       [:a.navbar-item {:href (routes/url-for :map)
+                        :class (when (= active-panel :map) "is-active")} "الخارطة"]]]]))
 
 (defn top
   [language]
