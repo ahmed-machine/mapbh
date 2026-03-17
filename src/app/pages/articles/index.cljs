6a143fb8b6a00fc7f305c9764223e49f743296d7
ahmed-machine <ahmed-machine@users.noreply.github.com>
2026-03-17 01:51:52 -0400

    cleanup index entry for open data

--- src/app/pages/articles/index.cljs
+++ src/app/pages/articles/index.cljs
@@ -35,14 +35,14 @@
     :date #inst "2026-02-06T00:00:00.000Z"
     :route "processing-pipeline"
     :component processing-pipeline/article}
-   {:en-title "the politics of open data"
-    :ar-title "the politics of open data"
-    :en-subtitle "mapping bahrain: the politics of open data"
-    :ar-subtitle "mapping bahrain: the politics of open data"
-    :en-description "As we mark five years of mapBH, a look at the structural barriers this project set out to overcome and the open data model we built."
-    :ar-description "As we mark five years of mapBH, a look at the structural barriers this project set out to overcome and the open data model we built."
+   {:en-title "mapping bahrain"
+    :ar-title "\u062e\u0631\u0627\u0626\u0637 \u0627\u0644\u0628\u062d\u0631\u064a\u0646"
+    :en-subtitle "the politics of open data"
+    :ar-subtitle "\u0633\u064a\u0627\u0633\u0629 \u0627\u0644\u0628\u064a\u0627\u0646\u0627\u062a \u0627\u0644\u0645\u0641\u062a\u0648\u062d\u0629"
+    :en-description "Looking back at five years of mapBH, the landscape, and successes."
+    :ar-description "\u062e\u0645\u0633 \u0633\u0646\u0648\u0627\u062a \u0645\u0646 mapBH\u060c \u0627\u0644\u0635\u0639\u0648\u0628\u0627\u062a \u0648\u0627\u0644\u0627\u0646\u062c\u0627\u0632\u0627\u062a"
     :en-keywords ["article" "open data" "mapBH" "Bahrain" "open source" "maps"]
-    :ar-keywords ["article" "open data" "mapBH" "Bahrain" "open source" "maps"]
+    :ar-keywords ["\u0645\u0642\u0627\u0644" "\u0628\u064a\u0627\u0646\u0627\u062a \u0645\u0641\u062a\u0648\u062d\u0629" "mapBH" "\u0627\u0644\u0628\u062d\u0631\u064a\u0646" "\u0645\u0635\u062f\u0631 \u0645\u0641\u062a\u0648\u062d" "\u062e\u0631\u0627\u0626\u0637"]
     :date #inst "2026-03-17T00:00:00.000Z"
     :route "open-data"
     :component open-data/article}])
